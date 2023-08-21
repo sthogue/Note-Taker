0071de80bf8055ea2c24e10f8f5b8700390c5745
sthogue <sthogue16@gmail.com>
2023-08-21 12:44:02 -0500

    added router, uuid dependency, & updated server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const path = require('path');
 const db = require('./db/db.json');
+const api = require('./routers/index.js');
+const { v4: uuidv4 } = require('uuid');
 // Initialize an instance of Express.js
 const app = express();
 
@@ -21,19 +23,42 @@ app.get('/notes', (req, res) =>
 );
  
 // This method sends a JSON response with the correct content type.
-app.get('/api', (req, res) => res.json(db));
-
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+app.get('/api/notes', (req, res) => res.json(db));
 
 // POST method route
 app.post('/api/notes', (req, res) => {
   res.json(`${req.method} request received to add a note`);
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      review_id: uuidv4(),
+    };
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
 });
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+
 
 // listen() method is responsible for listening for incoming connections on the specified port 
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
 
